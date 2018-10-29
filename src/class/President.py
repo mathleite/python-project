b60752292c3c00b73eeb24df775f34a0fbc6a89b
matheussantosleite_@hotmail.com <matheussantosleite_@hotmail.com>
2018-10-29 13:34:04 -0300

    Up file

---
+++ src/class/President.py
@@ -0,0 +1,10 @@
+class President:
+    name = str(None)
+    name = str(None)
+    name = str(None)
+
+    def set_name(self, name):
+        self.name = name
+
+    def get_name(self):
+        return self.name
