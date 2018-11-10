3987c6e78ab8803f27b2a16ba864d7c32bdbd7d9
silvaadriel <silvaadriel@hotmail.com>
2018-11-10 09:55:19 -0200

    __init__ method included

--- src/class/president.py
+++ src/class/president.py
@@ -1,6 +1,10 @@
 class President:
     name = str(None)
 
+    def __init__(self, name, bank):
+        self.set_name(name)
+        self.bank_president = bank
+
     def set_name(self, name):
         self.name = name
 
